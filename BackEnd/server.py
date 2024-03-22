a765c6b3c48eb03c6895db437efb86b3346a0c64
ww-samuel-hale <samuel.hale@woodwing.com>
2024-03-22 15:12:59 -0400

    Added database connection and test endpoints

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,12 +1,25 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+CORS(app)
+
+db = SQLAlchemy(app)
+
+### TEST API ENDPOINTS ###
+@app.route('/api/test_db', methods=['GET'])
+def test_db():
+    try:
+        db.session.query(text("1")).from_statement(text("SELECT 1")).all()
+        return jsonify({'message': 'Database connection successful.'}), 200
+    except Exception as e:
+        return jsonify({'message': 'Database connection failed.', 'error': str(e)}), 500
 
 @app.route('/api/hello', methods=['GET'])
 def get_hello():
